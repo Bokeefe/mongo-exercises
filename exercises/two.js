46d8579bcad00e7345006859cbf8cd2e0b5bcc7b
Bokeefe <brndnokf@yahoo.com>
2016-10-19 09:32:44 -0600

    almost

--- exercises/two.js
+++ exercises/two.js
@@ -1,21 +1,16 @@
 module.exports = function(mongoose, Checkout, Movie) {
 	// Which users checked out any of the Lord of the Rings trilogy?
+	var movieQuery = /^The Lord of the Rings/;
 
-// 		var uniqueId = [];
-// 		var movieQuery = /^Lord of the Rings/;
-// 		//Kitten.find({ name: /^fluff/ }, callback);
-// 		Movie.find(
-// //object? with every instnace of Lotr
-
-// 			,
-// //object? with every userId
-
-
-
-	};
-
-
-	
-
-	
-
+	Movie.find({title:/^The Lord of the Rings/},function(err, result){
+		for(var i = 0; i < result.length; i++){
+			var movieNums = result[i].id;
+				console.log (movieNums);
+			Movie.findOne({_id:movieNum},function(err,result){//why it hate the err?
+				if (err) {
+					console.log("not work");
+					} console.log(result.userId);
+			});
+		}
+	});
+};
